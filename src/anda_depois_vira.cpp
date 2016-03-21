7fecfe0393f0984011afe9736fe901be45280204
leticiamaki <leticiamaki3d22@gmail.com>
2016-03-21 09:55:24 -0300

    go2point

--- src/anda_depois_vira.cpp
+++ src/anda_depois_vira.cpp
@@ -1,48 +1,49 @@
+//NÃO TERMINEI
+
 #include<ros/ros.h>
 #include<std_msgs/Float64.h>
 #include<geometry_msgs/Twist.h>
 #include<nav_msgs/Odometry.h>
 #include<geometry_msgs/Quaternion.h>
-#include<geometry_msgs/Point.h>
 #include<tf/tf.h>
 
 ros::Publisher pub;
 
-
-void Callback(const nav_msgs::OdometryConstPtr &msg){  // tipo de mensagem que a callback recebe
-    //declaração de variáveis
-    double yaw, d, teta, xf = 4, yf = 1.5, deltaTeta, k = 1, r = 0.1, v, w, x, y; 
-    
+void Callback(const nav_msgs::OdometryConstPtr &msg){
+    double yaw, d, teta, xf = 4, yf = 1.5, deltaTeta, k = 1, r = 0.1, v, w, x, y, delta_y; 
     geometry_msgs::Quaternion qt;
     geometry_msgs::Point position;
-    
     geometry_msgs::Twist twist_msg; 
     
-    //Equações
-    qt = msg->pose.pose.orientation;  // pegando posição
-    yaw = tf::getYaw(qt);             //só quero o yaw
-    ROS_INFO("VALUE YAW: %f", yaw);   // imprimindo yaw 
-    
-    position= msg->pose.pose.position;  //pegando posição x e y
+    qt = msg->pose.pose.orientation;
+    position= msg->pose.pose.position;
+    yaw = tf::getYaw(qt);
+    ROS_INFO("VALUE YAW: %f", yaw);   
     
     d = sqrt(pow(xf - position.x, 2) + pow(yf - position.y, 2));
     teta = atan2(yf - position.y, xf - position.x);
     deltaTeta = teta - yaw;
-    ROS_INFO("DELTA TETA: %F", deltaTeta);
     
-    if (d > 0.2){     // ainda não chegou
-        if(deltaTeta < -0.055 || deltaTeta > 0.055){    // o valor do w é proporcional ao deltaTeta
+    ROS_INFO("DELTA TETA: %F", deltaTeta);
+    delta_y = position.y - yf;    
+    
+    if (delta_y > 1){     // ainda não chegou
+        v = 1;
+        w = 0;
+    }
+    else{   // encontrou a posição de chegada
+        if(deltaTeta < -0.055 || deltaTeta > 0.055){
             v = 0;   
-            w = 0.8 * deltaTeta;                        //alinhando o carro com o angulo
+            w = 0.6 * deltaTeta;
         }
-        else{
-            v = 1;                                     // já alinhado, avance 
+        if(position.x - xf < 0.2 || position.x - xf > -0.2){
+            v = 0;
             w = 0;
         }
-    }
-    else{                                              // encontrou a posição de chegada
-        v = 0;
-        w = 0;
+        else{
+            v = 1;  
+            w = 0;
+        }
     } 
     
     //w = k * deltaTeta;
@@ -67,4 +68,3 @@ int main(int argc, char **argv){
     
     return 0;
 }
-
